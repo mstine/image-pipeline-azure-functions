0ae1851c07320613d43b8d9f65ce902a6e132f74
Matt Stine <matt.stine@gmail.com>
2017-10-27 11:07:33 -0400

    use api differently based on successful httpie call

--- analyze-image/index.js
+++ analyze-image/index.js
@@ -1,20 +1,21 @@
 var NEEDLE = require('needle');
 
+/*
+
+http -f post https://southcentralus.api.cognitive.microsoft.com/vision/v1.0/describe Ocp-Apim-Subscription-Key:8b03b56769db430c832bcbd33f07c062 maxCandidates==1 Content-Type:
+application/octet-stream < ~/Downloads/DevOps-Pivotal-Office-SF-01\ \(1\).jpg
+
+*/
+
 module.exports = function(context, inputBlob) {
 
   var options = {
-      "Ocp-Apim-Subscription-Key" : "8b03b56769db430c832bcbd33f07c062"
+      "Ocp-Apim-Subscription-Key" : "8b03b56769db430c832bcbd33f07c062",
+      "Content-Type" : "application/octet-stream"
   };
 
-  var data = {
-    payload : {
-      value: inputBlob,
-      content_type: 'application/octet-stream'
-    }
-  };
-
-  NEEDLE.post('https://southcentralus.api.cognitive.microsoft.com/vision/v1.0/describe',
-    data, options, function(err, response) {
+  NEEDLE.post('https://southcentralus.api.cognitive.microsoft.com/vision/v1.0/describe?maxCandidates=1',
+    inputBlob, options, function(err, response) {
         if (err) {
           console.log(err);
         } else {

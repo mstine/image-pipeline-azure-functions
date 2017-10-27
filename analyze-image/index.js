ce91a1b7ffdae5eb9fb0c8632a7073c273d96044
Matt Stine <matt.stine@gmail.com>
2017-10-27 09:40:03 -0400

    initial commit

---
+++ analyze-image/index.js
@@ -0,0 +1,25 @@
+var NEEDLE = require('needle');
+
+module.exports = function(context, inputBlob) {
+
+  var options = {
+      "Ocp-Apim-Subscription-Key" : "8b03b56769db430c832bcbd33f07c062"
+  };
+
+  var data = {
+    payload : {
+      value: inputBlob,
+      content_type: 'application/octet-stream'
+    }
+  };
+
+  NEEDLE.post('https://southcentralus.api.cognitive.microsoft.com/vision/v1.0/describe',
+    data, options)
+    .then(response => {
+        console.log(response.Result);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+}
